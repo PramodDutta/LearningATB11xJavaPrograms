5baf3050c145e7c393865470be03ea1210016375
Promode <pramoddutta_96@yahoo.com>
2025-04-08 08:05:47 +0530

    8th APRIL 2025

--- src/ex_25_OOPs_Abstraction_Interface/Lab202_Abstraction.java
+++ src/ex_25_OOPs_Abstraction_Interface/Lab202_Abstraction.java
@@ -13,10 +13,22 @@ class ConcreateClass implements Incomplete_Interface{
 }
 
 interface Incomplete_Interface{
-    int a = 10;
+    int a = 10; // final
     void display();
+
+    default void display2(){
+        System.out.println("Default is allowed!");
+    }
+
+    static void display3(){
+        System.out.println("Default is allowed!");
+    }
 }
 
 abstract class Incomplete_abstract{
+    int a= 10;
     abstract void display1();
+    void display2(){
+
+    }
 }
